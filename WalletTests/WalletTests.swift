026ff2c93283fccf293ffc87b12a9f9f9cf028fe
Vinn <kevgun03@gmail.com>
2025-08-20 19:45:33 +0700

    Initial Commit

---
+++ WalletTests/WalletTests.swift
@@ -0,0 +1,16 @@
+//
+//  WalletTests.swift
+//  WalletTests
+//
+//  Created by Vin on 20/08/25.
+//
+
+import Testing
+
+struct WalletTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
